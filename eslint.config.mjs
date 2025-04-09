b9f1fb944a4e0f19bb6aec263122fa14fa1e5094
william <william.intan@unimelb.edu.au>
2025-04-09 15:43:02 +1000

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
@@ -8,4 +8,9 @@ export default defineConfig([
   { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.browser } },
   { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
   tseslint.configs.recommended,
+  globalIgnores([
+    'cdk.out/',
+    'node_modules/*',
+    'app/*', // App folder should have its own eslint config
+  ]),
 ]);
